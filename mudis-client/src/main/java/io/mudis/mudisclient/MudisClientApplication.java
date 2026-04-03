2b52e6099953afacd8bcd1d98cffb56c36296949
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-04-04 00:42:52 +0400

    chore: update dependencies and clean up Spring Shell configuration
    - upgrade Spring Boot to 4.0.5
    - upgrade Netty to 4.2.12.Final and replace netty-all with modular deps
    - remove unnecessary spring-boot-starter and spring-shell-jline
    - remove custom PromptProvider
    - ${version} -> ${project.version}
    - update Maven compiler plugin
    - fix LICENSE year

--- mudis-client/src/main/java/io/mudis/mudisclient/MudisClientApplication.java
+++ mudis-client/src/main/java/io/mudis/mudisclient/MudisClientApplication.java
@@ -1,11 +1,7 @@
 package io.mudis.mudisclient;
 
-import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.shell.jline.PromptProvider;
 
 @SpringBootApplication(
         proxyBeanMethods = false,
@@ -18,13 +14,5 @@ public class MudisClientApplication {
 
     static void main(String[] args) {
         SpringApplication.run(MudisClientApplication.class, args);
-    }
-
-    @Bean
-    public PromptProvider myPromptProvider() {
-        return () -> new AttributedString(
-                "mudis-client:> ",
-                AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW)
-        );
     }
 }
